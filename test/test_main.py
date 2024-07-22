6db64d49e3d1553a344abd419e176b525f43c1bb
Ziwei Li <heinzt@mail.ustc.edu.cn>
2024-07-22 23:03:35 +0800

    decoder change commit

--- test/test_main.py
+++ test/test_main.py
@@ -2,16 +2,19 @@ import sys
 sys.path.append("src")
 
 import verilogtools
+import pandas as pd
+import torch
 
 def main():
-    df1 = verilogtools.eda_rpt_analyze.NewPandas(
-        RptPath="test\\top_module.mapped.area.rpt",
-        mode="area",
-        start=39,
-        end=8424
+    verilogtools.classic_rtl_export.decoder(
+        filename="example.v",
+        mode="bitmask",
+        sigIn="x",
+        sigOut="y",
+        widthIn=5,
+        reverse=True,
+        indent_num=2,
     )
-
-    df1.to_excel("example.xlsx")
 
 if __name__ == '__main__':
     main()
