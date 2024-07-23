c8ffa7116048413f6077cfbaa8308e00059e7a57
Ziwei Li <heinzt@mail.ustc.edu.cn>
2024-07-23 13:29:58 +0800

    eda report commit1

--- src/verilogtools/eda_rpt_analyze/rptreader.py
+++ src/verilogtools/eda_rpt_analyze/rptreader.py
@@ -1,10 +1,59 @@
+from typing import (
+    Optional,
+    Literal,
+)
 import pandas as pd
 import re
 
-def ReadRpt(RptPath:str) -> list[str]:
-    with open(RptPath, 'r') as f:
-        lines = f.readlines()
-    return lines
+class EdaReport():
+    def __init__(self, rptPath: Optional[str]) -> None:
+        self.lines = None
+        if rptPath is None:
+            pass
+        else:
+            self.loadRpt(rptPath)
+
+    def loadRpt(self, rptPath: str) -> list[str]: 
+        self.lines = self.readRpt(rptPath)
+        for line in self.lines:
+            tokens = line.strip().split()
+
+    def readRpt(self, rptPath: str) -> list[str]:
+        with open(rptPath, 'r') as f:
+            lines = f.readlines()
+        return lines
+    
+    def getReportAttributes(self, lines: list[str]):
+        report = {}
+        attri = iter([
+            'Report',
+            'Design',
+            'Version',
+            'Date',
+            'Dataframe'
+        ])
+
+        target = next(attri, 'end')
+        for line in lines:
+            if target == 'end':
+                break
+            tokens = re.sub(r'\:' ,'' , line.strip()).split()
+            if tokens is []:
+                continue
+            elif tokens[0] == target:
+                if target == 'Date':
+                    report[target] = {
+                        'week': tokens[1],
+                        'month': tokens[2],
+                        'day': tokens[3],
+                        'time': tokens[4],
+                        'year': tokens[5]
+                    }
+                else:
+                    report[target] = tokens[1]
+            else:
+                pass
+
 
 def InfoChoose(line:str, mode:str) -> list[str]:
     token = re.sub(r'\(.*?\)' ,"" ,line).strip().split()
--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,19 @@
 import sys
 sys.path.append("src")
-
+from typing import Literal
 import verilogtools
 import pandas as pd
 import torch
 
 def main():
+    lst = ["a", "b", "c"]
+    i = iter(lst)
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i, 'end'))
+    exit()
+
     verilogtools.classic_rtl_export.decoder(
         filename="example.v",
         mode="bitmask",
